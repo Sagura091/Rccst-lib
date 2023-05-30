3d2af00fa57aa2a9a2a8eaebbabc17e89d6d7966
sagura091 <blakealbertbergstrom@gmail.com>
2023-05-30 10:44:04 -0400

    misc bh fix

--- rccstui/rccst_ui_component/atom/miscellaneous.cljs
+++ rccstui/rccst_ui_component/atom/miscellaneous.cljs
@@ -4,9 +4,9 @@
             [rccst-ui-component.atom.example.misc.hex-color-picker :as hex-picker]
             [rccst-ui-component.atom.example.misc.rgba-color-picker :as rgba-picker]
             [rccst-ui-component.atom.example.misc.two-d-globe :as two-d-globe]
-            [rccst-ui-component.atom.example.misc.bh-table :as bh-table]
-            [rccst-ui-component.atom.example.misc.meta-bh-table :as meta-bh-table]
-            [rccst-ui-component.atom.example.misc.meta-coc-bh-table :as meta-coc-bh-table]
+            [rccst-ui-component.atom.example.misc.bh.bh-table :as bh-table]
+            [rccst-ui-component.atom.example.misc.bh.meta-bh-table :as meta-bh-table]
+            [rccst-ui-component.atom.example.misc.bh.meta-coc-bh-table :as meta-coc-bh-table]
             [rccst-ui-component.atom.example.misc.worldwind.globe :as ww-globe]
             [rccst-ui-component.atom.example.misc.data-table :as data-table]))
 
